41bbf2b6eeaf42dddc99bd9a82bb006ea50386d6
omri <omrip500@gmail.com>
2022-08-31 00:58:07 +0300

    cancel EmailField + Email validator - more try

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 # WTForm
