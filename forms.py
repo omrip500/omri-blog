13f932ceb0831ce8333f4be733dd84498d008829
omri <omrip500@gmail.com>
2022-08-30 23:38:03 +0300

    cancel Email Validator in RegisterForm - different method

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
-import email_validator
 from flask_ckeditor import CKEditorField
 
 # WTForm
