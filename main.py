3b2fb1c57500471a6c958f33fbb0794b15c16de6
omri <omrip500@gmail.com>
2022-09-03 21:10:20 +0300

    edit post requiremtns changing

--- main.py
+++ main.py
@@ -237,8 +237,8 @@ def add_new_post():
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-# @admin_required
-@blog_owner_required
+@admin_required
+# @blog_owner_required
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
