3d09ed449d9e85ab82bf08db4a2a1c9fd80748c8
omri <omrip500@gmail.com>
2022-09-04 11:22:30 +0300

    condition in edit_post instead of decorator

--- main.py
+++ main.py
@@ -240,9 +240,10 @@ def add_new_post():
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 # @admin_required
-@blog_owner_required
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
+    if post.author != current_user:
+        return abort(403)
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
